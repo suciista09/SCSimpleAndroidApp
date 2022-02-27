5494d2d971ffbda68b5aac80885fa46ce38a5b57
suciista09 <suci.ij@gmail.com>
2022-02-27 16:01:12 +0700

    change name of app

--- sc-simple-android-app/src/androidTest/java/com/example/first_android/ExampleInstrumentedTest.kt
+++ sc-simple-android-app/src/androidTest/java/com/example/first_android/ExampleInstrumentedTest.kt
@@ -17,7 +17,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+        // Context of the sc-simple-android-app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.first_android", appContext.packageName)
     }
